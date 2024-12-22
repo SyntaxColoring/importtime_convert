6461d334ffab9a600043b016de637c75b16af3d5
Max Marrone <max@marrone.nyc>
2024-12-22 00:56:11 -0500

    Add a test that parses the current interpreter's actual output.

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import dataclasses
 import io
+import subprocess
+import sys
 import typing
 
 import pytest
@@ -26,7 +28,12 @@ import time:       555 |        666 | foo._bar.baz
 
 
 def test_junk_filtering() -> None:
-    """Test that junk interspersed with the `-X importtime` output is ignored."""
+    """Test that junk interspersed with the `-X importtime` output is ignored.
+
+    We expect to be able to ignore junk that occupies a full line, and junk that
+    occupies the beginning of a line. The latter can happen if something does
+    `print(sys.stderr, "blah", end=None)`.
+    """
     input = """\
 blah
 import time: self [us] | cumulative | imported package
@@ -126,6 +133,69 @@ import time:         0 |          0 | unittest.mock
     ]
 
     assert simplified_result == expected_result
+
+
+@pytest.mark.parametrize(
+    "packages",
+    [
+        ["re"],
+        ["asyncio"],
+        ["unittest.mock"],
+        ["import_time_flame_graph"],
+        ["re", "asyncio", "unittest.mock", "import_time_flame_graph"],
+    ],
+)
+def test_live(packages: list[str]) -> None:
+    """Try parsing the actual `-X importtime` output of the current Python interpreter.
+
+    The intent here is to catch if the `-X importtime` output format changes in
+    some future Python version.
+    """
+    raw_output = _do_x_importtime(packages)
+    raw_output_lines = raw_output.splitlines()
+
+    # We expect the first line to be a header:
+    header_line = raw_output_lines[0]
+    assert "import time" in header_line
+    assert "self" in header_line
+    assert "cumulative" in header_line
+    assert "imported package" in header_line
+
+    # Extract the "imported package" part of each row, including indentation, e.g.
+    # foo
+    #   bar
+    # baz
+    raw_output_package_column = [line.split("|")[-1] for line in raw_output_lines[1:]]
+
+    def depth_first_packages_and_levels(
+        imports: list[Import], root_level: int = 0
+    ) -> typing.Iterator[tuple[str, int]]:
+        for import_ in imports:
+            yield from depth_first_packages_and_levels(import_.children, root_level + 1)
+            yield import_.package, root_level
+
+    # From the result of our parsing, reconstruct what the original "imported package"
+    # column ought to have looked like, including indentation. It should match exactly.
+    parsed = parse(io.StringIO(raw_output))
+    parsed_packages_and_levels = depth_first_packages_and_levels(parsed)
+    reconstructed_package_column = [
+        " " * (level * 2 + 1) + package
+        for (package, level) in parsed_packages_and_levels
+    ]
+
+    assert reconstructed_package_column == raw_output_package_column
+
+
+def _do_x_importtime(packages: list[str]) -> str:
+    python_command = ";".join(f"import {package}" for package in packages)
+    assert sys.executable
+    cli_command = [sys.executable, "-X", "importtime", "-c", python_command]
+    output = subprocess.run(
+        cli_command,
+        capture_output=True,
+        check=True,
+    )
+    return output.stderr.decode("ascii")
 
 
 def _simplify_tree(tree: list[Import]) -> list[_SimplifiedNode]:
